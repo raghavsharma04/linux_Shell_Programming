06b67d919d8891e54d8de228658def3ac41cb449
raghavsharma04 <raghav.45-cse-19@mietjammu.in>
2022-11-09 21:40:44 +0530

    experiment 3

---
+++ experiment3.sh
@@ -0,0 +1,5 @@
+#Write a shell script to see current date, time username and directory
+echo "Write a shell script to see current date, time username and directory"
+echo "Today's date is: `date`"
+echo "Username is `whoami`"
+echo "The current directory is `pwd`"
