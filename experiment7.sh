f38f49de82ffef71616010c221c8fe5697189881
raghavsharma04 <raghav.45-cse-19@mietjammu.in>
2022-11-09 21:45:33 +0530

    experiment 7

---
+++ experiment7.sh
@@ -0,0 +1,19 @@
+#Write a script to check whether the given number is prime or not
+
+echo "Write a script to check whether the given number is prime or not"
+echo "enter a number"
+read num
+function prime
+{
+for((i=2; i<=num/2; i++))
+do
+  if [ $((num%i)) -eq 0 ]
+  then
+    echo "$num is not a prime number."
+    exit
+  fi
+done
+echo "$num is a prime number."
+}
+r=`prime $number`
+echo "$r"
