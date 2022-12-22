fa47c2c69e624f58af25e9df003da20975516940
Raghav Sharma <62952976+raghavsharma04@users.noreply.github.com>
2022-12-22 22:36:49 +0530

    experiment10

---
+++ experiment10.sh
@@ -0,0 +1,3 @@
+#Write a shell script to print file names in directory showing date of creation & serial no. of file.
+echo "Write a shell script to print file names in directory showing date of creation & serial no. of file."
+echo `ls -l`
